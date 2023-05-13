a0a1d3c2911981de0bd8e5c08fc79f733b4480a5
Yutthasat Phoncharoenpong <pyutthasat@gmail.com>
2023-05-14 02:36:57 +0700

    user api done V2

--- controllers/UsersListController.js
+++ controllers/UsersListController.js
@@ -29,34 +29,3 @@ exports.updateUser = function(req, res) {
         res.json(Users)
     })
 }
-
-exports.readContact= function(req,res){
-    console.log(req.params.id)
-    User.findById(req.params.id, function(err,Contacts){
-        if(err) throw err
-        res.json(Contacts)
-    })
-}
-
-exports.deleteContact = function(req,res){
-    console.log(req.params.id)
-    User.findByIdAndRemove(req.params.id, function(err, Contacts){
-        if(err) throw err
-        const response = {
-            message: "Delete user id : " + req.params.id,
-            id: Contacts._id
-        }
-        res.json(response)
-    })
-}
-
-exports.updateContact = function(req,res){
-    console.log(req.params.id)
-    var newUser = {}
-    newUser = req.body
-    User.findByIdAndUpdate(req.params.id, newUser, {new: true}, function(err, Contacts){
-        if(err) throw err
-        console.log(Contacts)
-        res.json(Contacts)
-    }) 
-}
